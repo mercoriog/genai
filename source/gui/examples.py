619bec0154c28500f0570412835bf4d5b9f1c64a
mercoriog <98778953+mercoriog@users.noreply.github.com>
2024-04-10 16:39:47 +0200

    xml introduction for examples

--- source/gui/examples.py
+++ source/gui/examples.py
@@ -1,38 +1,35 @@
 from PIL import Image
+import xml.etree.ElementTree as ET
 
-# TODO: gestisci l'accesso agli index ed automatizza la creazione degli esempi e l'apertura delle immagini corrispondenti
-examples_images = [
-    Image.open("genai\\media\\generated_images\\male_blonde_blue_arman1jacket.png"),
-    Image.open("genai\\media\\generated_images\\female_blonde_blue_arman1jacket.png"), 
-    Image.open("genai\\media\\generated_images\\male_ginger_blue_arman1jacket.png")
-]
-
-examples_list = [
-	[
-		examples_images[0],
-		"Male",
-    	"Blonde", 
-    	"Blue", 
-    	"GIORGIO ARMANI, full body photo of blonde male model wearing dark gray jacket, arman1jacket:1.2, realistic face, extremely high quality RAW photograph, detailed background, intricate, Exquisite details and textures, highly detailed, ultra detailed photograph, warm lighting, 4k, sharp focus, high resolution, detailed skin, detailed eyes, 8k UHD, DSLR, high quality, film grain, Fujifilm XT3, luxury walk in a street of paris", 
-    	"(worst quality, greyscale), ac_neg2, zip2d_neg, ziprealism_neg, watermark, username, signature, text, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, jpeg artifacts, bad feet, extra fingers, mutated hands, poorly drawn hands, bad proportions, extra limbs, disfigured, bad anatomy, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, mutated hands, fused fingers, too many fingers, long neck"
-	],[
-    	examples_images[1],
-    	"Female", 
-    	"Blonde", 
-    	"Blue", 
-    	"GIORGIO ARMANI, full body photo of blonde female model wearing dark gray jacket, arman1jacket:1.2, (realistic face:1.4), extremely high quality RAW photograph, detailed background, intricate, Exquisite details and textures, (highly detailed:1.2), sharp focus, (ultra detailed photograph:1.2), warm lighting, 4k,  high resolution, (detailed skin:1.2), (detailed eyes:1.2), 8k UHD, DSLR, high quality, film grain, Fujifilm XT3, private party, Venice beach in California, summer day, fashion show", 
-    	"(worst quality, greyscale), ac_neg2, zip2d_neg, ziprealism_neg, watermark, username, signature, text, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, jpeg artifacts, bad feet, extra fingers, mutated hands, poorly drawn hands, bad proportions, extra limbs, disfigured, bad anatomy, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, mutated hands, fused fingers, too many fingers, long neck, black and white filter"
-    ],[
-	    examples_images[2],
-	    "Male", 
-        "Ginger", 
-	    "Blue", 
-        "GIORGIO ARMANI, full body photo of ginger male model wearing dark gray jacket, arman1jacket:1.2, realistic face, extremely high quality RAW photograph, detailed background, intricate, Exquisite details and textures, sharp focus, highly detailed, ultra detailed photograph, warm lighting, 4k,  high resolution, detailed skin, detailed eyes, 8k UHD, DSLR, high quality, film grain, Fujifilm XT3, private party on yatch, summer days", 
-        "(worst quality, greyscale), ac_neg2, zip2d_neg, ziprealism_neg, watermark, username, signature, text, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, jpeg artifacts, bad feet, extra fingers, mutated hands, poorly drawn hands, bad proportions, extra limbs, disfigured, bad anatomy, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, mutated hands, fused fingers, too many fingers, long neck"
-    ]
-]
+EXAMPLES_XML_PATH = "genai\\source\\examples.xml"
 
 def getExamples():
+	examples_list = []
+	ex_tree = ET.parse(EXAMPLES_XML_PATH)
+	ex_root = examples_tree.getroot()
+
+	# scroll the entire tree
+	for ex in ex_root.findall("example"):
+		# retrive every subelement
+		ex_img_url = ex.findtext("image-url")
+		ex_gender = ex.findtext("gender")
+		ex_hair_color = ex.findtext("hair-color")
+		ex_eyes_color = ex.findtext("eyes-color")
+		ex_positive_prompt = ex.findtext("positive-prompt")
+		ex_negative_prompt = ex.findtext("negative-prompt")
+		
+		# load the actual image
+		ex_img = Image.open(ex_img_url)
+
+		# create a list 
+		examples_list.append([
+			ex_img,
+			ex_gender,
+			ex_hair_color,
+			ex_eyes_color,
+			ex_positive_prompt,
+			ex_negative_prompt
+		])
 	return examples_list
 
 def getExamplesInfo():
