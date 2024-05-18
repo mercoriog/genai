a94e79b138897ac3045a587694a5ba051f203091
GIANLUIGI MERCORIO <mercoriogianluigi@gmail.com>
2024-05-18 11:54:36 +0200

    update bat script

--- start.bat
+++ start.bat
@@ -6,13 +6,13 @@ REM Get dirname from script abspath.
 set "script_dir=%~dp0"
 
 REM Set my virtual environment name.
-set "virtualenv_name=genai_env"
+set "virtualenv_name=env"
 
 REM Set correct directory where to store 'genaienv'.
-set "venv_path=%script_dir%\%virtualenv_name%"
+set "venv_path=%script_dir%%virtualenv_name%"
 
 REM Activate virtual environment.
 echo [START] Start 'genai.py'...
-%venv_path%\Scripts\python %script_dir%\src\main.py
+%venv_path%\Scripts\python %script_dir%src\main.py
 
 
