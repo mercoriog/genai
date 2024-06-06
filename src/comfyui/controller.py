db51a768b870266f25ee9de112a51b935b4cb809
GIANLUIGI MERCORIO <98778953+mercoriog@users.noreply.github.com>
2024-06-06 09:05:02 +0200

    updates

--- src/comfyui/controller.py
+++ src/comfyui/controller.py
@@ -1,35 +1,8 @@
-from comfyui import output as comfyOUT
 from comfyui import comfyURL
 from comfyui import workflow as comfyWORK
 from comfyui import script
 from comfyui import prompt
 from gallery import controller as gal
-
-def generateRemoteImage(positive_prompt, negative_prompt):
-    # This function generate a new image using positive and negative prompt.
-    # It process a request to comfyui API using both prompts
-    # and check for new generated image stored in output folder.
-
-    # Get ComfyUI API URL.
-    URL = comfyURL.getURL()
-
-    # Get workflow file.
-    workflow_file = comfyWORK.getWorkflowFilePath()
-
-    # Update workflow file with positive and negative prompt.
-    prompt_workflow = comfyWORK.updateWorkflow(positive_prompt, negative_prompt, workflow_file)
-
-    # Get ComfyUI output folder.
-    output_folder = comfyOUT.getOutputFolderPath()
-
-    # Perform remote generation with:
-    # <URL> of the target machine, 
-    # <prompt> for the body of generation,
-    # <output_folder> to retrive generated image.
-    image = script.remoteGeneration(prompt_workflow, URL, output_folder)
-
-    # Return generated image (can be None if an error occurs).
-    return image
 
 def generateWebSocketImage(positive_prompt, negative_prompt):
     # This function generate a new image using positive and negative prompt.
--- src/comfyui/script.py
+++ src/comfyui/script.py
@@ -1,68 +1,6 @@
-from comfyui import output as comfyOUT
 from comfyui import websocket as comfyWS
 import websocket #NOTE: websocket-client (https://github.com/websocket-client/websocket-client)
-import json
-import requests
 import uuid
-
-def startRemoteQueue(prompt_workflow, URL):
-    # Set an exception handler:
-    try:
-        # Build request body.
-        body = {"prompt": prompt_workflow}
-
-        # Encode body to process request.
-        data = json.dumps(body).encode('utf-8')
-
-        # Process request.
-        requests.post(URL, data=data)
-
-    # An error occurs:
-    except Exception as e:
-        # Print the exception.
-        print("[ERROR] " + str(e))
-
-        # Return False if error occurs.
-        return None
-
-    # If no error occurs, return True
-    return True
-
-def remoteGeneration(prompt, URL, output_folder):
-    # Set an exception handler:
-    try:
-        # Get latest image in folder before generating new one 
-        # and storing it in the same folder.
-        # This latest image will be the [OLD]_latest_image.
-        previous_image = comfyOUT.getLatestImage(output_folder)
-
-        # Call the API to generate the image.
-        startRemoteQueue(prompt, URL)
-
-        # Process until the new generated image is stored in output folder.
-        while True:
-            # Check for latest image.
-            latest_image = comfyOUT.getLatestImage(output_folder)
-            
-            # Check if current latest image correspond to [OLD]_latest_image: 
-            if latest_image != previous_image:
-                
-                # If False, <latest_image> is the new generated image.
-                return latest_image 
-
-            # Setting a waiting time to looking for new generated image.
-            time.sleep(1)
-
-        # By default return the [OLD]_latest_image.
-        return previous_image
-
-    # An error occurs:
-    except Exception as e:
-        # Print the exception.
-        print("[ERROR] " + str(e))
-
-        # Return False if error occurs.
-        return None
 
 def webSocketGeneration(prompt_workflow, URL):
     # Set an exception handler:
